4b2a20d85d29f88e8a15b1a181d617fcd912fd87
Marco Carvalho <marcocarvalho.web@gmail.com>
2023-11-03 11:13:22 -0300

    Simple Security

--- app/src/main/java/application/model/User.java
+++ app/src/main/java/application/model/User.java
@@ -1,14 +1,20 @@
 package application.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
+    private String role;
 
     public long getId() {
         return id;
@@ -28,4 +34,11 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
 }
--- app/src/main/java/application/repositories/UserRepository.java
+++ app/src/main/java/application/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import application.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>  {
-    
+    User findByEmail(String email);
 }
