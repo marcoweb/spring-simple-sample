705055e5dc2d42c8882c5658a448b2ca483c833a
Marco Carvalho <marcocarvalho.web@gmail.com>
2023-11-03 11:48:43 -0300

    Using ByCrypt Password

--- app/src/main/java/application/config/SecurityConfig.java
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,9 +25,15 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @SuppressWarnings("deprecation")
     @Bean
-    public NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
+
+    // @SuppressWarnings("deprecation")
+    // @Bean
+    // public NoOpPasswordEncoder passwordEncoder() {
+    //     return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    // }
 }
--- assets/data/1-populate.sql
+++ assets/data/1-populate.sql
@@ -1,3 +1,4 @@
 USE application;
 
-INSERT INTO users(email, password) VALUES ('admin', 'root');
+-- INSERT INTO users(email, password) VALUES ('admin', 'root');
+INSERT INTO users(email, password) VALUES ('admin', '$2a$12$1GmHef9B8zUeLlLNbvLVCu.TgN4qFzzEUStH0IelVD9sVz.AdmhqC');
