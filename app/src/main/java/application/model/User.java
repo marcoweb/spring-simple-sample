0a48019e6b7831b5baea1b11cbac5bf79576928b
Marco Carvalho <marcocarvalho.web@gmail.com>
2023-11-03 09:48:37 -0300

    User Model

---
+++ app/src/main/java/application/model/User.java
@@ -0,0 +1,31 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private long id;
+    private String email;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
